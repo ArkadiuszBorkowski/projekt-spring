bbe92d0181cdef5637865d99ff822059cfe4be4b
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-08-27 20:40:11 +0200

    Serwis filmowy - strona glowna, ustawienia pom.xml, linquibase, modele filmu

---
+++ src/main/java/pl/javastart/movieclub/MovieclubApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.movieclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieclubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/movieclub/MovieclubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.movieclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieclubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
